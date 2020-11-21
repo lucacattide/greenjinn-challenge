01a5e181fb4c44327959c7b4fdcc20394636b3e7
Luca Cattide <info@lucacattide.dev>
2020-11-21 19:16:09 +0100

    feat: AppBar implementation

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,42 @@
 // Module Start
 // JS imports
-import Container from '@material-ui/core/Container';
+import {
+  Container,
+  AppBar,
+  Toolbar,
+  Typography
+} from '@material-ui/core';
+import {
+  makeStyles
+} from '@material-ui/core/styles';
 
+// Styles
+const useStyles = makeStyles(theme => ({
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    margin: '5em 0 0'
+  }
+}));
 // Layout
-const Layout = (props) => (
-  /* Container Start */
-  <Container>
-    {props.children}
-  </Container>
-  /* Container End */
-);
+const Layout = (props) => {
+  const classes = useStyles();
+
+  return (
+    <Container>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6">
+            GreenJinn Challenge
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main className={classes.content}>
+        {props.children}
+      </main>
+    </Container>
+  );
+};
 
 // Module export
 export default Layout;
