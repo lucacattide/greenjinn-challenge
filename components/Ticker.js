74b7a6de01fd817b7de7c562e38886df61222970
Luca Cattide <info@lucacattide.dev>
2020-11-21 19:16:54 +0100

    update: Ticker layout

--- components/Ticker.js
+++ components/Ticker.js
@@ -1,17 +1,29 @@
 // Module Start
 // JS imports
 import {
+  Slide,
   Card,
   CardHeader,
   CardContent,
   Typography
 } from '@material-ui/core';
+import { Update } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
+import moment from 'moment';
 
 // Styles
 const useStyles = makeStyles({
   card: {
     maxWidth: 345
+  },
+  flexItem: {
+    display: 'flex'
+  },
+  margin: {
+    margin: '1em 0 0'
+  },
+  updateIcon: {
+    margin: '0 16px 0 0'
   }
 });
 
@@ -20,12 +32,35 @@ const Ticker = () => {
   const classes = useStyles();
 
   return (
-    <Card className={classes.card}>
-      <CardHeader subheader="Average Ticker Values" />
-      <CardContent>
-        <Typography variant="h5">100</Typography>
-      </CardContent>
-    </Card>
+    <Slide
+      direction="up"
+      in={true}
+      mountOnEnter
+      unmountOnExit
+      timeout={{
+        enter: 300,
+        exit: 250
+      }}
+    >
+      <Card className={classes.card}>
+        <CardHeader subheader="Average Ticker Values" />
+        <CardContent>
+          <Typography variant="h4">฿ 100</Typography>
+          <Typography
+            variant="body2"
+            className={
+              `${classes.margin} ${classes.flexItem}`
+            }
+          >
+            <Update
+              fontSize="small"
+              className={classes.updateIcon}
+            />
+              {`Last update: ${moment().format('MM/DD/YYYY')}`}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Slide>
   );
 };
 
